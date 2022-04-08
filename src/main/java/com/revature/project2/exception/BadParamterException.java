92b681fad18376a427c939d0a255b1bd95a87ac5
jenobpj <jenobgit@gmail.com>
2022-04-08 01:19:07 -0700

    add new exception

---
+++ src/main/java/com/revature/project2/exception/BadParamterException.java
@@ -0,0 +1,9 @@
+package com.revature.project2.exception;
+
+public class BadParamterException  extends  Exception{
+
+    public BadParamterException(String message) {
+        super(message);
+    }
+}
+
