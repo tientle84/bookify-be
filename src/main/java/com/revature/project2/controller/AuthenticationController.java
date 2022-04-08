6570923423b44efd16a704666a14499da4acccc9
jenobpj <jenobgit@gmail.com>
2022-04-08 11:22:49 -0700

    add jwt dependecies and check the jwt token in controller

--- src/main/java/com/revature/project2/controller/AuthenticationController.java
+++ src/main/java/com/revature/project2/controller/AuthenticationController.java
@@ -5,8 +5,11 @@ import com.revature.project2.exception.BadParamterException;
 import com.revature.project2.model.LoginDTO;
 import com.revature.project2.model.User;
 import com.revature.project2.service.AuthenticationService;
+import com.revature.project2.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.security.auth.login.FailedLoginException;
 
 @RestController
+@CrossOrigin(originPatterns = "*",exposedHeaders = "*",allowedHeaders = "*")
 public class AuthenticationController {
 
     @Autowired
     private AuthenticationService authService;
 
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO dto){
         try {
             User user = authService.login(dto.getEmailId(), dto.getPassword());
-            return ResponseEntity.ok(user);
+             String jwt=jwtService.createJwt(user);
+
+            HttpHeaders responseHeaders =new HttpHeaders();
+            responseHeaders.set("token",jwt);
+            return ResponseEntity.ok().headers(responseHeaders).body(user);
         }catch (FailedLoginException e){
             return  ResponseEntity.status(401).body(e.getMessage());
         }catch( BadParamterException e ){
