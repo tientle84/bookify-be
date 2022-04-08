b29670b5638370da3a3c5a968170f9f90830b499
jenobpj <jenobgit@gmail.com>
2022-04-08 11:20:12 -0700

    add userJwtDto for store the jwt details

---
+++ src/main/java/com/revature/project2/model/UserJwtDto.java
@@ -0,0 +1,18 @@
+package com.revature.project2.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserJwtDto {
+    private int userId;
+    private String first_name;
+    private String last_name;
+    private String email;
+}
