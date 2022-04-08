91ccf0a506717c4008f26a7f35eff5d27a2b086f
jenobpj <jenobgit@gmail.com>
2022-04-08 01:19:41 -0700

    add new condition to service layer

--- src/main/java/com/revature/project2/service/AuthenticationService.java
+++ src/main/java/com/revature/project2/service/AuthenticationService.java
@@ -2,16 +2,28 @@ package com.revature.project2.service;
 
 
 import com.revature.project2.dao.UserRepository;
+import com.revature.project2.exception.BadParamterException;
 import com.revature.project2.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.security.auth.login.FailedLoginException;
+import javax.security.auth.login.LoginException;
 
 @Service
 public class AuthenticationService {
     @Autowired
     private UserRepository userRepository;
 
-    public User login(String emailId, String password){
-        return userRepository.findByEmailIdAndPassword(emailId,password);
+    public User login(String emailId, String password) throws FailedLoginException, BadParamterException {
+
+        if(emailId.trim().equals("") || password.trim().equals("")){
+            throw new BadParamterException("You must provide username and passwor to login");
+        }
+        User user= userRepository.findByEmailIdAndPassword(emailId.trim(),password.trim());
+        if(user == null){
+            throw new FailedLoginException("Invalid username and/or password");
+        }
+        return  user;
     }
 }
