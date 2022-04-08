c3e74fa53b07151c746459b5f3cc976127789f26
jenobpj <jenobgit@gmail.com>
2022-04-07 23:08:38 -0700

    change the port number

--- src/main/java/com/revature/project2/Project2Application.java
+++ src/main/java/com/revature/project2/Project2Application.java
@@ -1,13 +1,28 @@
 package com.revature.project2;
 
+import com.revature.project2.Utility.DataPopulateUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Project2Application {
+	@Autowired
+	private DataPopulateUtility util;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner test(){
+		return s -> {
+			util.populateInitialData();
+		};
 	}
 
 }
