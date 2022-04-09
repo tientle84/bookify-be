f6cdec5f0bf24fe7085829c6f1276eb88d30902e
jenobpj <jenobgit@gmail.com>
2022-04-09 10:08:37 -0700

    add register endpoint to register the user

--- src/main/java/com/revature/project2/controller/AuthenticationController.java
+++ src/main/java/com/revature/project2/controller/AuthenticationController.java
@@ -29,14 +29,19 @@ public class AuthenticationController {
     private JwtService jwtService;
 
 
+
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO dto) {
         try {
             User user = authService.login(dto.getEmailId(), dto.getPassword());
             String jwt = jwtService.createJwt(user);
 
+            UserDto userDto=new UserDto(user.getId(), user.getFirst_name(), user.getLast_name(), user.getEmailId(),
+                    user.getUserRole(), user.getPhone());
+
             HttpHeaders responseHeaders = new HttpHeaders();
             responseHeaders.set("token", jwt);
-            return ResponseEntity.ok().headers(responseHeaders).body(user);
+            return ResponseEntity.ok().headers(responseHeaders).body(userDto);
         } catch (FailedLoginException e) {
             return ResponseEntity.status(401).body(e.getMessage());
         } catch (BadParamterException e) {
@@ -45,11 +50,18 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public  ResponseEntity<?> register(@RequestBody UserDto dto){
+    public  ResponseEntity<?> register(@RequestBody User user){
         UserRole userRole=new UserRole(2,"bookRenter");
-        dto.setUserRole(userRole);
-        User user=authService.register(dto);
-        return  ResponseEntity.ok().body(user);
+        user.setUserRole(userRole);
+        User newUser=authService.register(user);
+
+        UserDto userDto=new UserDto(user.getId(), user.getFirst_name(), user.getLast_name(), user.getEmailId(),
+                user.getUserRole(), user.getPhone());
+
+        String jwt=jwtService.createJwt(newUser);
+        HttpHeaders responseHeaders=new HttpHeaders();
+        responseHeaders.set("token",jwt);
+        return  ResponseEntity.ok().headers(responseHeaders).body(userDto);
     }
 }
 
