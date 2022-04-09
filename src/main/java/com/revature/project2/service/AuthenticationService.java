1f55f5427661d4747c07f270ded6d8402552032c
jenobpj <jenobgit@gmail.com>
2022-04-09 10:09:23 -0700

    add register method for registering user

--- src/main/java/com/revature/project2/service/AuthenticationService.java
+++ src/main/java/com/revature/project2/service/AuthenticationService.java
@@ -4,12 +4,10 @@ package com.revature.project2.service;
 import com.revature.project2.dao.UserRepository;
 import com.revature.project2.exception.BadParamterException;
 import com.revature.project2.model.User;
-import com.revature.project2.model.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.FailedLoginException;
-import javax.security.auth.login.LoginException;
 
 @Service
 public class AuthenticationService {
@@ -28,8 +26,8 @@ public class AuthenticationService {
         return  user;
     }
 
-   public  User register(UserDto user){
-       //userRepository.save(user)
-       return  null;
+   public  User register(User user){
+      User newUser= userRepository.save(user);
+      return  newUser;
    }
 }
