6c947661e3dcd8f21fe64f84a0e5fbef5133d5eb
tientle84 <alctdl00@gmail.com>
2022-04-09 16:09:49 -0400

    update contextLoads to make the test pass

--- src/test/java/com/revature/project2/Project2ApplicationTests.java
+++ src/test/java/com/revature/project2/Project2ApplicationTests.java
@@ -2,12 +2,16 @@ package com.revature.project2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Project2ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
