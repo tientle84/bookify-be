cd1e7b27d39e576157533188eee643c8a3e59411
jenobpj <jenobgit@gmail.com>
2022-04-09 14:09:28 -0700

    add bookrepository

---
+++ src/main/java/com/revature/project2/dao/BookRepository.java
@@ -0,0 +1,18 @@
+package com.revature.project2.dao;
+
+import com.revature.project2.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+    public interface BookRepository extends JpaRepository<Book,Integer> {
+
+        public abstract List<Book> findAll();
+
+
+
+
+    }
+
