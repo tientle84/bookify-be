761596823ebd80fc691543524a1bf8ab058188e5
jenobpj <jenobgit@gmail.com>
2022-04-09 17:46:11 -0700

    fix the jwt bug and add authentication controll to the add book endpoint

--- src/main/java/com/revature/project2/controller/BookController.java
+++ src/main/java/com/revature/project2/controller/BookController.java
@@ -1,8 +1,12 @@
 package com.revature.project2.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.revature.project2.model.Book;
+import com.revature.project2.model.LoginDTO;
+import com.revature.project2.model.UserJwtDto;
 import com.revature.project2.service.BookService;
+import com.revature.project2.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +21,31 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private JwtService jwtService;
+
     @PostMapping("/addbook")
-    public ResponseEntity<?> addBook(@RequestParam("file")MultipartFile file,
+    public ResponseEntity<?> addBook(@RequestHeader("Authorization") String headerValue,
+                                     @RequestParam("file")MultipartFile file,
                                      @RequestParam("isbn") String isbn,
                                      @RequestParam("title") String title,
                                      @RequestParam("author") String author,
                                      @RequestParam("publisher")String publisher,
                                      @RequestParam("date")String date,
                                      @RequestParam("genre") String genre
-                                     ) {
-        Book book=bookService.addBook(file,isbn,title,author,publisher,date,genre);
-         return ResponseEntity.ok().body(book);
+                                     )  {
 
+        String jwt=headerValue.split(" ")[1];
+        try{
+            UserJwtDto dto=jwtService.parseJwt(jwt);
+            System.out.println(dto);
+            if(dto.getUserRole().equals("liberian")){
+                Book book=bookService.addBook(file,isbn,title,author,publisher,date,genre);
+                return ResponseEntity.ok().body(book);
+            }
+           return ResponseEntity.status(404).body("You are not Authorized to access this end point ");
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
-
 }
--- src/main/java/com/revature/project2/service/JwtService.java
+++ src/main/java/com/revature/project2/service/JwtService.java
@@ -19,20 +19,29 @@ public class JwtService {
     private Key key;
 
     public JwtService(){
-        byte[] secret="my_secret_passwordhkjhkgkgjhghjgkjgkgfgftufut".getBytes();
+        byte[] secret="my_secret_passwordhkjhkgkgjhghjgkjgkgfgftufutfdsfsdfsdfgsdgsdgdgsdg".getBytes();
         key= Keys.hmacShaKeyFor(secret);
     }
 
-    public  String createJwt(User user){
+    public  String createJwt(User user) throws JsonProcessingException {
+
+        UserJwtDto dto=new UserJwtDto(user.getId(),user.getFirst_name(),user.getLast_name(),user.getEmailId(),
+                user.getUserRole().getUserRole());
+
         String jwt= Jwts.builder()
-                .claim("user_dto",new UserJwtDto(user.getId(),user.getFirst_name(),user.getLast_name(),user.getEmailId(),user.getUserRole()))
+                .claim("user_dto",new ObjectMapper().writeValueAsString(dto))
                 .signWith(key)
                 .compact();
         return  jwt;
     }
+
+
     public UserJwtDto parseJwt(String jwt) throws JsonProcessingException {
-      Jws<Claims> token=  Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
-       String dtoString=(String) token.getBody().get("user_dto");
-       return (new ObjectMapper().readValue(dtoString,UserJwtDto.class));
+
+     Jws<Claims> token =Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
+
+     String dtoString = (String) token.getBody().get("user_dto");
+
+     return (new ObjectMapper()).readValue(dtoString, UserJwtDto.class);
     }
 }
